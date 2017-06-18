1ffc8d3f50cb15482968beae1bb7a73ab96a94c7
juanma-perez <jmpt1994@hotmail.com>
2017-06-18 10:00:49 -0500

    Maquillado del crawler/intento en python

--- python-service/Scrapy/crawler2.py
+++ python-service/Scrapy/crawler2.py
@@ -1,18 +1,21 @@
 import wikipedia 
 import pyScraper
 import pyWiki
+import Libraries.FileManager as fm
 
-print pyWiki.politic_scrapeTable("https://es.wikipedia.org/wiki/Juan_Manuel_Santos")
+
 wikipedia.set_lang("es")
+dic = { }
 def obtenerPaginas(persona):
 	print "Relaciones Familiares: "
 	print
 	page = wikipedia.page(persona)
 	imprimirLinks(page.url,persona)
+	fm.writeFileJSON("test",dic)
 	print
 	print "Relaciones Laborales: "
 	print	
-	info = pyScraper.politic_scrapeTable(page.url, "")
+	info = pyScraper.politic_scrapeTable(page.url)
 	if info.has_key("laboral - links"):
 		for item in info["laboral - links"]:
 			for link in item:
@@ -23,17 +26,22 @@ def obtenerPaginas(persona):
 visitadas = set()
 
 def imprimirLinks(url,persona):	
-	info = pyScraper.politic_scrapeTable(url, "")
+	info = pyScraper.politic_scrapeTable(url)
+	dic[persona.encode("utf-8")] = {}
 	if info.has_key("Familia"):
 		for item in info["Familia"]:
 			if "links" in item:
 				for link in info["Familia"][item]:
 					if item.strip(" - links").encode("utf-8")+ link["url"].encode("utf-8") not in visitadas:
 						if "#cite" not in link["url"]:
-							print persona.encode("utf-8")+" "+ item.strip(" - links").encode("utf-8")+" "+link["title"].encode("utf-8")+" "+link["url"].encode("utf-8")
+							relacion =  item.strip(" - links").encode("utf-8")
+							if dic[persona.encode("utf-8")].has_key(relacion):
+								dic[persona.encode("utf-8")][relacion].append(link["url"].encode("utf-8"))
+							else:
+								dic[persona.encode("utf-8")][relacion] = [link["url"].encode("utf-8")]
+							#print persona.encode("utf-8")+" "+ item.strip(" - links").encode("utf-8")+" "+link["title"].encode("utf-8")+" "+link["url"].encode("utf-8")
 							visitadas.add(item.strip(" - links").encode("utf-8")+ link["url"].encode("utf-8"))
 							imprimirLinks(link["url"] ,link["title"])
-		
 
 
-#obtenerPaginas("Alvaro Uribe")
+obtenerPaginas("Juan Manuel Santos Calderon")
