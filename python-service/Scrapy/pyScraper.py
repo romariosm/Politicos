49d4f1b1adcbffb731b107419aa92153a742a2c4
Romario Sanchez <romariosanchez@ticsocial.com.co>
2017-06-24 17:55:47 -0500

    estandaracion parcial del crawler

--- python-service/Scrapy/pyScraper.py
+++ python-service/Scrapy/pyScraper.py
@@ -6,6 +6,7 @@ import re
 import time
 import Libraries.FileManager as fm
 import Libraries.JSONProcessor as jsonp
+import json
 # -*- coding: utf-8 -*-
 
 #Returns a Beutiful object
@@ -50,20 +51,32 @@ def politic_scrapeTable(url):
 					dic = jsonp.addValue(dic,parent,{})
 				elif len(fil.find_all('th')) > 0  and len(fil.find_all('td')) > 0:
 					if parent != "":
-						dic[parent] = jsonp.addValue(dic[parent],fil.find_all('th')[0].text, jsonp.clearValue(fil.find_all('td')[0].text))
+						
+						data_clean=jsonp.clearValue(''.join(value for value in fil.find_all(text=True) if value.parent.name != 'a' and value.parent.name != 'th' and value != '' and value != ' ' and value != '\n' ))
+						#print data_clean, "--------"
+						if hasattr(data_clean, '__iter__'):
+							if len(data_clean)>0:
+								dic[parent]=jsonp.addValue(dic[parent], fil.find_all('th')[0].text, [{'title': text, 'url': None } for text in data_clean if text != '' and text != ''])
+						elif data_clean != '':
+							dic[parent]=jsonp.addValue(dic[parent], fil.find_all('th')[0].text, [{'title': data_clean, 'url': None }])
+						 
 						if len(fil.find_all('td')[0].findAll('a'))>0:
-							print 
-							print fil.find_all('th')[0].text
-							print getLinks(fil.find_all('td')[0])
-							print
-							dic[parent] = jsonp.addValue(dic[parent],fil.find_all('th')[0].text + " - links", getLinks(fil.find_all('td')[0]))
+							
+							if dic[parent].has_key(jsonp.eliminateCharacters(fil.find_all('th')[0].text)):
+
+								for link in getLinks(fil.find_all('td')[0]):
+									print link.get('url')
+									if link.get('url'):
+										dic[parent][fil.find_all('th')[0].text].append(link)																
+							else:
+								dic[parent]=jsonp.addValue(dic[parent], fil.find_all('th')[0].text, getLinks(fil.find_all('td')[0]))
 					else:
 						dic = jsonp.addValue(dic,fil.find_all('th')[0].text, fil.find_all('td')[0].text)		
 	except Exception as error:
 	   	fm.registerError(str(error))
 	return dic 
-#politic_scrapeTable("https://es.wikipedia.org/wiki/Juan_Manuel_Santos","")
-#fm.writeFileJSON("prueba",politic_scrapeTable("https://es.wikipedia.org/wiki/Juan_Manuel_Santos", "no disponible"))
+
+#fm.writeFileJSON("prueba",politic_scrapeTable("https://es.wikipedia.org/wiki/Juan_Manuel_Santos"))
 
 
 def getLinks(element):
@@ -71,7 +84,10 @@ def getLinks(element):
 	for link in element.findAll('a'):
 		if "wikidata" not in link.get('href'):
 			enlace = {}
+			#print link
+			#print link.text
 			enlace["title"] = link.get('title')
+			#enlace["text"] =link.text
 			if "http" in link.get('href'):
 				enlace["url"] = link.get('href')
 			else:
@@ -79,4 +95,9 @@ def getLinks(element):
 			temp.append(enlace)
 	return temp
 
-fm.writeFileJSON('test_enri',politic_scrapeTable("https://es.wikipedia.org/wiki/Enrique_Santos_Castillo"))
+#fm.writeFileJSON('test_enri',politic_scrapeTable("https://es.wikipedia.org/wiki/Enrique_Santos_Castillo"))
+
+
+#parsed = json.loads()
+#print json.dumps(politic_scrapeTable("https://es.wikipedia.org/wiki/Juan_Manuel_Santos"), indent=4, sort_keys=True)
+print json.dumps(politic_scrapeTable("https://es.wikipedia.org/wiki/%C3%81lvaro_Uribe"), indent=4, sort_keys=True)
