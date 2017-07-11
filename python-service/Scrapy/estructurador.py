6a7d1cb7c8fd58921846bdf7e3cc400593c1b410
juanma-perez <jmpt1994@hotmail.com>
2017-07-10 22:05:54 -0500

    Creaciòn de la estructura a guardar en node

--- python-service/Scrapy/estructurador.py
+++ python-service/Scrapy/estructurador.py
@@ -1,21 +1,33 @@
 from pyScraper import *
 from pyOnotology import *
-
-structure = getStructure()
-synonyms = getSynonyms()
+import json
 def create_structure(table):
+	structure = getStructure()
+	synonyms = getSynonyms()
+
+	print table 
 	for key in table:
 		if isinstance(table[key],dict):
 			for key_2 in table[key]:
-				if isinstance(key_2,dict):
-					pass
-				else:
-					pass
-					#print key_2.encode('utf-8')
+				if type(table[key][key_2]) is list:
+					for item in table[key][key_2]:
+						print key_2.encode('utf-8')
+						print item #print key_2.encode('utf-8')
+						if synonyms.has_key(key_2.encode('utf-8')):
+							if item['title'] != None and item['url'] != None:
+								structure[synonyms[key_2.encode('utf-8')][0]][synonyms[key_2.encode('utf-8')][1]]=item
+							else:
+								if item['url'] != None:
+									structure[synonyms[key_2.encode('utf-8')][0]][synonyms[key_2.encode('utf-8')][1]]=item['url'].encode('utf-8')
 		else:
 			if key != "content" and type(table[key]) is not list:
+				print key.encode('utf-8')
 				if synonyms.has_key(key.encode('utf-8')):
 					structure[synonyms[key.encode('utf-8')][0]][synonyms[key.encode('utf-8')][1]]=table[key.encode('utf-8')]
-create_structure(politic_scrapeTable("https://es.wikipedia.org/wiki/Juan_Manuel_Santos"))
-print json.dumps(structure, indent=4, sort_keys=True)
+			elif type(table[key]) is list:
+				pass
+	return structure
+				
+print create_structure(politic_scrapeTable("https://es.wikipedia.org/wiki/Gustavo_Petro"))
+#json.dumps(structure, indent=4, sort_keys=True)
 
--- python-service/Scrapy/socket_python.py
+++ python-service/Scrapy/socket_python.py
@@ -3,7 +3,7 @@ from flask.ext.socketio import SocketIO, emit
 import pyWiki
 from pyScraper import *
 import Libraries.FileManager as fm
-
+from estructurador import *
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
@@ -36,6 +36,7 @@ def getDataSuggestion(message):
 	emit('suggestion dataResponse', pyWiki.getPageData(message))
 
 
+
 #@socketio.on('my broadcast event', namespace='/')
 #def test_message(message):
 #    emit('my response', {'data': message['data']}, broadcast=True)
@@ -50,3 +51,5 @@ def getDataSuggestion(message):
 
 if __name__ == '__main__':
     socketio.run(app)
+
+    
